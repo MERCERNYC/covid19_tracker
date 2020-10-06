d1ff49ce26e881e39ec6f13d3ea43363c484a19c
Juliana Fogg <julianafogg@gmail.com>
2020-10-05 23:45:33 -0400

    add material design

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,8 +1,25 @@
 import React from 'react';
+import { Card, CardContent, Typography, Grid } from '@material-ui/core';
+import styles from './Cards.module.css';
 
-const Cards = () => {
+
+const Cards = (props) => {
+    console.log(props);
+
     return(
-        <h1>Cards</h1>
+     <div className={styles.container}>
+     <Grid container spacing={3} justify='center'>
+        <Grid item component={Card}>
+         <CardContent>
+            <Typography color='textSecondary' gutterBottom>Infected</Typography>
+            <Typography variant='h5'> Real Data </Typography>
+            <Typography color='textSecondary' >Real Date</Typography>
+            <Typography variant='body2'> Number of Active casespf covid </Typography>
+         </CardContent>
+        </Grid>
+     </Grid>
+
+     </div>
     )
 }
 
