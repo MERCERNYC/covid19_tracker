fbebb3d2b57253034d7141665146ff9fc8637076
Juliana Fogg <julianafogg@gmail.com>
2020-10-10 22:55:49 -0400

    add daily data and linechart

--- src/api/index.js
+++ src/api/index.js
@@ -15,21 +15,11 @@ export const fetchData = async () => {
 }
 
 export const fetchDailyData = async () => {
-    
-    try{
-        const { data } = await axios.get(`${url}/daily`);
-    
-       
-        const modifiedData = data.map((dailyData) => ({
-            confirmed: dailyData.confirmed.total,
-            deaths: dailyData.deaths.total,
-            date: dailyData.reportDate,
-        }));
+    try {
+      const { data } = await axios.get('https://api.covidtracking.com/v1/us/daily.json');
 
-        return modifiedData;
-
-    }catch(error){
-
+      return data.map(({ positive, recovered, death, dateChecked: date }) => ({ confirmed: positive, recovered, deaths: death, date }));
+    } catch (error) {
+      return error;
     }
-
-}
+  };
--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -19,26 +19,35 @@ const Chart = () => {
          
     });
 
-    const lineChart = (
-      this.dailyData.length ? (
-          <Line
-            data={{
-              labels: dailyData.map(({ date }) => date),
-              datasets: [{
-                data: dailyData.map(({ confirmed }) => confirmed),
-                label: 'Infected',
-                borderColor: '#3333ff',
-                fill: true,
-              }, {
-                data: dailyData.map(({ deaths }) => deaths),
-                label: 'Deaths',
-                borderColor: 'red',
-                backgroundColor: 'rgba(255, 0, 0, 0.5)',
-                fill: true,
-              }],
-            }}
-          /> ) : null
-      );
+const lineChart = (
+    dailyData[0]  ? (
+      <Line
+        data={{
+          labels: dailyData.map(({ date }) => new Date(date).toLocaleDateString()),
+          datasets: [{
+            data: dailyData.map(({ confirmed }) => confirmed),
+            label: 'Infected',
+            borderColor: '#3333ff',
+            fill: true,
+            }, {
+            data: dailyData.map(({ deaths }) => deaths),
+            label: 'Deaths',
+            borderColor: 'red',
+            backgroundColor: 'rgba(255, 0, 0, 0.5)',
+            fill: true,
+            },  {
+            data: dailyData.map((data) => data.recovered),
+            label: 'Recovered',
+            borderColor: 'green',
+            backgroundColor: 'rgba(0, 255, 0, 0.5)',
+            fill: true,
+          },
+        ],
+      }}
+   />
+   ) : null
+ );
+
 
     return(
         <div className= {styles.container}>
