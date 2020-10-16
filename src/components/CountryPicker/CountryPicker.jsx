49c9caf55868f700cf15c5d84839f8e4380ddbfb
Juliana Fogg <julianafogg@gmail.com>
2020-10-16 02:38:07 -0400

    add form to global

--- src/components/CountryPicker/CountryPicker.jsx
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect} from 'react';
 import { NativeSelect, FormControl} from '@material-ui/core';
 import styles from './CountryPicker.module.css';
-import { countries } from '../../api';
+import { fetchCountries } from '../../api';
 
 
 const CountryPicker = () => {
@@ -9,10 +9,13 @@ const CountryPicker = () => {
     const [ fetchedCountries, setFetchedCountries] = useState([]);
 
     useEffect(()=>{
-        const fetchCountries = async () => {
-
+        const fetchAPI = async () => {
+            setFetchedCountries ( await fetchCountries() );
         }
-    })
+            fetchAPI();
+    },[setFetchedCountries]);
+    
+    console.log(fetchedCountries)
 
     return(
         <FormControl className={styles.formControl}>
